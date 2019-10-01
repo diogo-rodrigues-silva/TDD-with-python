7ce984e405013e6be33fdbb44931d88aaf0add87
Diogo Rodrigues <diogorodrigues0801@gmail.com>
2019-10-01 11:55:22 -0300

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -18,19 +18,18 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Diogo heard about a cool new online to-do app. He goes
+        # Diogo has heard about a cool new online to-do app. He goes
         # to check out its homepage
-        self.browser.get('http://127.0.0.1:8000')
-
+        self.browser.get('http://localhost:8000')
+        
         # He notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-
         # He is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_tag_name('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attribute('placeholder'),
+        inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
@@ -39,29 +38,30 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
 
         # When he hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
+        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_elements_by_id('id_list_table')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows)
         )
 
         # There is still a text box inviting him to add another item. He
-        # enter's "Use peacock feathers to make a fly" (Diogo is very methodical)
-        self.fail('Finish the test!!!')
+        # enters "Use peacock feathers to make a fly" (Diogo is very
+        # methodical)
+        self.fail('Finish the test!')
 
-    # The page updates again, and now shows both items an his list
+        # The page updates again, and now shows both items an his list
 
-    # Diogo wonders whethers the site will remember his list. Then he sees
-    # that the site has generated a unique URL for him -- there is some
-    # explanatory text to that effect
+        # Diogo wonders whethers the site will remember his list. Then he sees
+        # that the site has generated a unique URL for him -- there is some
+        # explanatory text to that effect
 
-    # He visits that URL - His to-do list is still there.
+        # He visits that URL - His to-do list is still there.
 
-    # Satisfied, he goes back to sleep
-    # browser.quit()
+        # Satisfied, he goes back to sleep
+        # browser.quit()
 
 
 if __name__ == "__main__":
